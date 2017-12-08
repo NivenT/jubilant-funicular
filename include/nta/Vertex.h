9684128399f4d6d17ff25e3397e02b5a880efdf4
NivenT <nachenjang@gmail.com>
2017-12-08 15:02:48 -0800

    added new constructor to Vertex2D

--- include/nta/Vertex.h
+++ include/nta/Vertex.h
@@ -7,23 +7,26 @@
 #define NUM_VERTEX_ATTRIBS 4
 
 namespace nta {
-    ///represents a vertex in 2 dimensions
+    /// represents a vertex in 2 dimensions
     struct Vertex2D {
-        ///Initializes an "empty" vertex
+        /// Initializes an "empty" vertex
         Vertex2D(){
         }
-        ///Initializes a white, textureless vertex with given position
+        /// Initializes a white, textureless vertex with given position
 		    Vertex2D(crvec2 p) : pos(p), color(glm::vec4(1)), uv(glm::vec2(0)), hasTexture(0.0) {
         }
-        ///Initialized a vertex with everything
-		    Vertex2D(crvec2 p, crvec4 c, crvec2 u, float t = 1.0) : pos(p),color(c),uv(u),hasTexture(t) {
+        /// Initialize textureless, colorful vertex
+        Vertex2D(crvec2 p, crvec4 c) : pos(p), color(c), uv(glm::vec2(0)), hasTexture(0.0) {
         }
-        ///sets the position of the vertex
+        /// Initialized a vertex with everything
+		    Vertex2D(crvec2 p, crvec4 c, crvec2 u, float t = 1.0) : pos(p), color(c), uv(u), hasTexture(t) {
+        }
+        /// sets the position of the vertex
         void setPosition(float x, float y) {
             pos.x = x;
             pos.y = y;
         }
-        ///sets the color of the vertex
+        /// sets the color of the vertex
         void setColor(float r, float g, float b, float a) {
             color.r = r;
             color.g = g;
@@ -36,12 +39,12 @@ namespace nta {
             color.b = c.b;
             color.a = 1;
         }
-        ///sets the uv coordinates of the vertex
+        /// sets the uv coordinates of the vertex
         void setUV(float u, float v) {
             uv.s = u;
             uv.t = v;
         }
-        ///the vertex's position, color, and uv coordinates, respectively
+        /// the vertex's position, color, and uv coordinates, respectively
         glm::vec2 pos;
         glm::vec4 color;
         glm::vec2 uv;
