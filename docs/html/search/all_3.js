8916fb82a0e4f0f4f9feca471f9152703019cb25
NivenT <nachenjang@gmail.com>
2017-12-19 13:42:25 -0800

    updated docs

--- docs/html/search/all_3.js
+++ docs/html/search/all_3.js
@@ -5,6 +5,6 @@ var searchData=
   ['depth',['depth',['../d6/d4f/structnta_1_1Primitive.html#a4443e427fb512898693bba61a8ac4654',1,'nta::Primitive::depth()'],['../d6/db3/structnta_1_1Glyph.html#a259a2061c71dfd9fa0857f52d4daff8a',1,'nta::Glyph::depth()']]],
   ['destroy',['destroy',['../da/d29/classnta_1_1AudioManager.html#aa337e5cc1825c6e2b971e1fc173b17ce',1,'nta::AudioManager::destroy()'],['../d0/dc4/classnta_1_1ScreenManager.html#ad4e28094d9293b55f38ea463ac802219',1,'nta::ScreenManager::destroy()']]],
   ['draw',['draw',['../df/d9f/classnta_1_1ParticleBatch2D.html#a270e573b2a767bf32fde34d17d838e69',1,'nta::ParticleBatch2D::draw()'],['../d1/de9/classnta_1_1ParticleEngine2D.html#a759937cf45e74162ce85269ddf59237b',1,'nta::ParticleEngine2D::draw()']]],
-  ['drawtext',['drawText',['../d6/d57/classnta_1_1SpriteFont.html#a86e0d5974a15cb477355bed3d38fc932',1,'nta::SpriteFont']]],
+  ['drawtext',['drawText',['../d6/d57/classnta_1_1SpriteFont.html#afdd646f19b11d9c8128151afad994a77',1,'nta::SpriteFont']]],
   ['drawtexture',['drawTexture',['../d6/d57/classnta_1_1SpriteFont.html#ac5e60e0a7b6f9c0302c81f0d5b6628cc',1,'nta::SpriteFont']]]
 ];
