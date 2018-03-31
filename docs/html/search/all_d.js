4646bec4fb161f5f2ac3cf2e14c817da6dcbac63
NivenT <nachenjang@gmail.com>
2018-03-30 17:59:18 -0700

    updated docs

--- docs/html/search/all_d.js
+++ docs/html/search/all_d.js
@@ -2,6 +2,5 @@ var searchData=
 [
   ['offfocus',['offFocus',['../d6/d23/classnta_1_1Screen.html#a172d24c2224aef718d151b621379def1',1,'nta::Screen']]],
   ['offset',['offset',['../dd/d38/structnta_1_1RenderBatch.html#ab73ef79a5cbf8b60ad9991eb0a1ea9e6',1,'nta::RenderBatch']]],
-  ['onfocus',['onFocus',['../d6/d23/classnta_1_1Screen.html#af29956d6f8ac437ef2f26c9d0973e509',1,'nta::Screen']]],
-  ['operator_3c_3c',['operator&lt;&lt;',['../d4/d51/namespacenta.html#a957c53a80ce60ea94277aa028afa0684',1,'nta']]]
+  ['onfocus',['onFocus',['../d6/d23/classnta_1_1Screen.html#af29956d6f8ac437ef2f26c9d0973e509',1,'nta::Screen']]]
 ];
--- docs/html/search/functions_d.js
+++ docs/html/search/functions_d.js
@@ -1,6 +1,5 @@
 var searchData=
 [
   ['offfocus',['offFocus',['../d6/d23/classnta_1_1Screen.html#a172d24c2224aef718d151b621379def1',1,'nta::Screen']]],
-  ['onfocus',['onFocus',['../d6/d23/classnta_1_1Screen.html#af29956d6f8ac437ef2f26c9d0973e509',1,'nta::Screen']]],
-  ['operator_3c_3c',['operator&lt;&lt;',['../d4/d51/namespacenta.html#a957c53a80ce60ea94277aa028afa0684',1,'nta']]]
+  ['onfocus',['onFocus',['../d6/d23/classnta_1_1Screen.html#af29956d6f8ac437ef2f26c9d0973e509',1,'nta::Screen']]]
 ];
