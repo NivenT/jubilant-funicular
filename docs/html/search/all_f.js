2c686a778a2de31c04cec67237af55e1cbc009b1
NivenT <nachenjang@gmail.com>
2018-04-29 16:31:04 -0700

    updated docs

--- docs/html/search/all_f.js
+++ docs/html/search/all_f.js
@@ -11,6 +11,7 @@ var searchData=
   ['releasekey',['releaseKey',['../d6/de2/classnta_1_1InputManager.html#acb2f11b5580141d4c3345499eea485c0',1,'nta::InputManager']]],
   ['render',['render',['../dc/dbe/classnta_1_1DebugBatch.html#a3188990f4fd9aeede13573fe00bd0cf4',1,'nta::DebugBatch::render()'],['../d4/df3/classnta_1_1PrimitiveBatch.html#a899411db2d7abc313d8bef4a19fd097e',1,'nta::PrimitiveBatch::render()'],['../d6/d23/classnta_1_1Screen.html#a323ff61ac31f55bd39fa818ccd8200fb',1,'nta::Screen::render()'],['../d4/d60/classnta_1_1Sprite.html#a01e831cda46d73acd8a4eff3cb56d5d3',1,'nta::Sprite::render()'],['../d7/d3b/classnta_1_1SpriteBatch.html#ae7566d38b0cdd33f2b3e16dfcf993714',1,'nta::SpriteBatch::render()']]],
   ['renderbatch',['RenderBatch',['../dd/d38/structnta_1_1RenderBatch.html',1,'nta::RenderBatch'],['../dd/d38/structnta_1_1RenderBatch.html#a02a531d942296d7194ca96f7ed614f14',1,'nta::RenderBatch::RenderBatch()']]],
+  ['resize',['resize',['../d4/dfb/classnta_1_1Window.html#a23b55b95d017fe35655ed1287869bdf1',1,'nta::Window']]],
   ['resourcemanager',['ResourceManager',['../d5/d87/classnta_1_1ResourceManager.html',1,'nta']]],
   ['resume',['resume',['../d4/dab/classnta_1_1Music.html#a7eb8159db905f34ebf6ad48ec9a1afa1',1,'nta::Music']]],
   ['rotate',['rotate',['../d3/d28/classnta_1_1Camera2D.html#a4529f2111c2621764c42365df5a504e0',1,'nta::Camera2D::rotate()'],['../d4/d51/namespacenta.html#ada7b30da347b63962acfdc8f16c1ffb8',1,'nta::rotate()']]],
