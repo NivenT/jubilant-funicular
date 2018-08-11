c5d63e8991fd835cc639e1749200172d7dd8fb75
NivenT <nachenjang@gmail.com>
2018-08-11 01:47:37 -0400

    updated docs

--- docs/html/search/functions_11.js
+++ docs/html/search/functions_11.js
@@ -2,10 +2,10 @@ var searchData=
 [
   ['threadpool',['ThreadPool',['../df/d5b/classnta_1_1utils_1_1ThreadPool.html#a3336e668eb283fcb65654058a9ed9113',1,'nta::utils::ThreadPool']]],
   ['timer',['Timer',['../d6/dd4/classnta_1_1Timer.html#ac92420dd62672b399f1b4adfd81a4007',1,'nta::Timer']]],
-  ['to_5fstring',['to_string',['../d1/d23/classnta_1_1utils_1_1JsonNum.html#a7038acc17c49e0162d6462dc0591a82d',1,'nta::utils::JsonNum::to_string()'],['../d7/d33/namespacenta_1_1utils.html#a5f35430518156d9cedd06866a50035c3',1,'nta::utils::to_string()']]],
+  ['to_5fstring',['to_string',['../d1/d23/classnta_1_1utils_1_1JsonNum.html#a7038acc17c49e0162d6462dc0591a82d',1,'nta::utils::JsonNum::to_string()'],['../d7/d33/namespacenta_1_1utils.html#aab201e3eaa7a7d050ca81ca029ec2347',1,'nta::utils::to_string()']]],
   ['tobinary',['toBinary',['../d3/d18/classnta_1_1Compressor.html#affed498c6ed43db813de5c46892b9163',1,'nta::Compressor']]],
   ['tokenize',['tokenize',['../d1/d3d/classnta_1_1utils_1_1Json.html#a905d67b125a7aadd771bb74a1bb63f34',1,'nta::utils::Json']]],
   ['toprimitivetype',['toPrimitiveType',['../d4/df3/classnta_1_1PrimitiveBatch.html#aaa6f5af45276d6562599e82ef430a8ce',1,'nta::PrimitiveBatch']]],
   ['translatecenter',['translateCenter',['../d3/d28/classnta_1_1Camera2D.html#a711b1b64b4e0bbca5c598ac15609f498',1,'nta::Camera2D']]],
-  ['trim',['trim',['../d7/d33/namespacenta_1_1utils.html#a9b119483690a21ce86d99d61106ee412',1,'nta::utils']]]
+  ['trim',['trim',['../d7/d33/namespacenta_1_1utils.html#a5dcdd9caf6995a33370beb952759866f',1,'nta::utils']]]
 ];
