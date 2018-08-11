90affffe4bae43720693fd276557022096a62b52
NivenT <nachenjang@gmail.com>
2018-08-11 01:47:20 -0400

    fixed types in util functions

--- include/nta/utils.h
+++ include/nta/utils.h
@@ -9,15 +9,15 @@
 namespace nta {
 	namespace utils {
 		/// Checks if str starts with prefix
-		extern bool starts_with(crstring& str, crstring& prefix);
+		extern bool starts_with(crstring str, crstring prefix);
 		/// Checks if str ends with suffix
-		extern bool ends_with(crstring& str, crstring& suffix);
+		extern bool ends_with(crstring str, crstring suffix);
 		/// Replaces all occurences of o in str with n
-		extern std::string replace_all(crstring& str, crstring o, crstring n);
+		extern std::string replace_all(crstring str, crstring o, crstring n);
 		/// Replaces all occurences of any string in the ith element of os with the ith element of ns
-		extern std::string replace_all(crstring& str, const std::vector<std::vector<std::string>> os, const std::vector<std::string> ns);
+		extern std::string replace_all(crstring str, const std::vector<std::vector<std::string>>& os, const std::vector<std::string>& ns);
 		/// Removes leading and trailing whitespace
-		extern std::string trim(crstring& str);
+		extern std::string trim(crstring str);
 		/// Splits a string into substrings separated by delim
 		extern std::vector<std::string> split(crstring str, char delim);
 		/// Rotates a point (about the origin) by the given angle
@@ -25,7 +25,7 @@ namespace nta {
 
 		/// converts input to a std::string
 	    template<class T>
-	    std::string to_string(T input, std::size_t precision = 0) {
+	    std::string to_string(const T& input, std::size_t precision = 0) {
 	        std::ostringstream os;
 	        if (precision != 0) {
 	            os.precision(precision);
--- src/utils.cpp
+++ src/utils.cpp
@@ -2,13 +2,13 @@
 
 namespace nta {
     namespace utils {
-        bool starts_with(crstring& str, crstring& prefix) {
+        bool starts_with(crstring str, crstring prefix) {
             return str.find(prefix) == 0;
         }
-        bool ends_with(crstring& str, crstring& suffix) {
+        bool ends_with(crstring str, crstring suffix) {
             return str.size() >= suffix.size() && str.rfind(suffix) == str.size() - suffix.size();
         }
-        std::string replace_all(crstring& str, crstring o, crstring n) {
+        std::string replace_all(crstring str, crstring o, crstring n) {
             std::string ret = str;
 
             int pos = 0;
@@ -18,7 +18,7 @@ namespace nta {
             }
             return ret;
         }
-        std::string replace_all(crstring& str, const std::vector<std::vector<std::string>> os, const std::vector<std::string> ns) {
+        std::string replace_all(crstring str, const std::vector<std::vector<std::string>>& os, const std::vector<std::string>& ns) {
             assert(os.size() == ns.size());
 
             std::string ret = str;
@@ -30,7 +30,7 @@ namespace nta {
             }
             return ret;
         }
-        std::string trim(crstring& str) {
+        std::string trim(crstring str) {
             static const std::string whitespace = " \t\n\v\f\r\0";
 
             auto begin = str.find_first_not_of(whitespace);
@@ -38,14 +38,14 @@ namespace nta {
 
             return str.substr(begin, end-begin+1);
         }
-        std::vector<std::string> split(crstring& str, char delim) {    
-            std::vector<std::string> elems; 
+        std::vector<std::string> split(crstring str, char delim) {    
+            std::vector<std::string> items; 
             std::stringstream ss(str);  
             std::string item;   
             while (std::getline(ss, item, delim)) { 
-                elems.push_back(item);  
+                items.push_back(item);  
             }   
-            return elems;   
+            return items;   
         }
         glm::vec2 rotate(crvec2 pt, float angle) {
         return glm::vec2(glm::cos(angle)*pt.x - glm::sin(angle)*pt.y,
