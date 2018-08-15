a853961837f903ef5d2069e1396d193d9ed6edf3
NivenT <nachenjang@gmail.com>
2018-08-15 01:45:13 -0400

    cosmetic changes to random.h

--- include/nta/Random.h
+++ include/nta/Random.h
@@ -1,5 +1,5 @@
-#ifndef RANDOM_H
-#define RANDOM_H
+#ifndef NTA_RANDOM_H_INCLUDED
+#define NTA_RANDOM_H_INCLUDED
 
 #include <random>
 
@@ -8,28 +8,28 @@
 #endif // RAND_MAX
 
 namespace nta {
-    ///Used for generating random numbers
+    /// Used for generating random numbers
     class Random {
     private:
         static std::default_random_engine   generator;
     public:
-        ///initializes random number generation
+        /// initializes random number generation
         static void init();
-        ///randomly returns true or false
+        /// randomly returns true or false
         static bool randBool() {return rand()%2==0;}
-        ///returns a random int in the specified range exclusive (uniform distribution)
+        /// returns a random int in the specified range exclusive (uniform distribution)
         static long randInt(long min, long max);
         static long randInt(long max) {return randInt(0,max);}
         static long randInt() {return randInt(0,RAND_MAX);}
-        ///returns a random float in the specified range (uniform distribution)
+        /// returns a random float in the specified range (uniform distribution)
         static float randFloat(float min, float max);
         static float randFloat(float max) {return randFloat(0,max);}
         static float randFloat() {return randFloat(0,1);}
-        ///returns a random float using the specified distribution
+        /// returns a random float using the specified distribution
         static float randGaussian(float mean, float sd);
-        ///returns the random number generator
+        /// returns the random number generator
         static std::default_random_engine getRNG() {return generator;}
     };
 }
 
-#endif // RANDOM_H
+#endif // NTA_RANDOM_H_INCLUDED
