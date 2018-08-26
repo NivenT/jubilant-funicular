ab64405e71e4f88ca250b43f50d26cb09b2d24d6
NivenT <nachenjang@gmail.com>
2018-08-26 14:28:10 -0400

    Added comments

--- include/nta/IDFactory.h
+++ include/nta/IDFactory.h
@@ -17,13 +17,22 @@ namespace nta {
 			T m_next_id;
 		public:
 			IDFactory() : m_next_id(0) {}
+			/// Resets this to a new IDFactory
 			void clear() { m_free = std::stack<T>(); m_next_id = 0; }
+			/// Resets this to a new IDFactory
+			void reset() { clear(); }
 
+			/// Generates a new, unused ID
 			T gen_id();
+			/// Marks an id as free so that it can be reused
+			///
+			/// id must not already be free
 			void free_id(T id);
 
+			/// Returns the number of active ids
 			std::size_t get_count() const { return m_next_id - m_free.size(); }
 
+			/// Calls (and returns) gen_id
 			T operator()() { return gen_id(); }
 		};
 
