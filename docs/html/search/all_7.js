165a11bff705d3bfc3a01acb65670754932f4940
NivenT <nachenjang@gmail.com>
2018-08-29 17:35:24 -0400

    updated docs

--- docs/html/search/all_7.js
+++ docs/html/search/all_7.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['handle_5ferrors',['handle_errors',['../de/db1/classnta_1_1ErrorManager.html#adca13bc3b6b36366c0a803ce43daf90d',1,'nta::ErrorManager']]],
   ['handleinput',['handleInput',['../d6/d23/classnta_1_1Screen.html#a645ccabe314f6c9084b493617c3eeafe',1,'nta::Screen']]],
-  ['has_5fcomponent',['has_component',['../db/da0/classnta_1_1ECS.html#acc0017328e58c4046747abf984ef1df1',1,'nta::ECS']]],
+  ['has_5fcomponent',['has_component',['../db/da0/classnta_1_1ECS.html#a370204d3b816a67fe22147f00d0c9447',1,'nta::ECS']]],
   ['haschildren',['hasChildren',['../da/dbb/classnta_1_1HuffmanNode.html#a429374a0e433063935f6e86816be8831',1,'nta::HuffmanNode']]],
   ['hash_3c_20nta_3a_3aentity_20_3e',['hash&lt; nta::Entity &gt;',['../d3/df6/structstd_1_1hash_3_01nta_1_1Entity_01_4.html',1,'std']]],
   ['hash_3c_20nta_3a_3agltexture_20_3e',['hash&lt; nta::GLTexture &gt;',['../d3/dcf/structstd_1_1hash_3_01nta_1_1GLTexture_01_4.html',1,'std']]],
--- docs/html/search/functions_7.js
+++ docs/html/search/functions_7.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['handle_5ferrors',['handle_errors',['../de/db1/classnta_1_1ErrorManager.html#adca13bc3b6b36366c0a803ce43daf90d',1,'nta::ErrorManager']]],
   ['handleinput',['handleInput',['../d6/d23/classnta_1_1Screen.html#a645ccabe314f6c9084b493617c3eeafe',1,'nta::Screen']]],
-  ['has_5fcomponent',['has_component',['../db/da0/classnta_1_1ECS.html#acc0017328e58c4046747abf984ef1df1',1,'nta::ECS']]],
+  ['has_5fcomponent',['has_component',['../db/da0/classnta_1_1ECS.html#a370204d3b816a67fe22147f00d0c9447',1,'nta::ECS']]],
   ['haschildren',['hasChildren',['../da/dbb/classnta_1_1HuffmanNode.html#a429374a0e433063935f6e86816be8831',1,'nta::HuffmanNode']]],
   ['huffmanleaf',['HuffmanLeaf',['../d0/d39/classnta_1_1HuffmanLeaf.html#afcd113b293d90d3297d409421bbfb285',1,'nta::HuffmanLeaf::HuffmanLeaf()'],['../d0/d39/classnta_1_1HuffmanLeaf.html#a68e98537fe635d83cc04a2ce70e99993',1,'nta::HuffmanLeaf::HuffmanLeaf(GLubyte data, int freq)']]],
   ['huffmannode',['HuffmanNode',['../da/dbb/classnta_1_1HuffmanNode.html#a6e92a7ee38fd56994c2eb2d894d3d108',1,'nta::HuffmanNode::HuffmanNode()'],['../da/dbb/classnta_1_1HuffmanNode.html#ae9aa27c2ed1cd215e3fe6a5fc48a0527',1,'nta::HuffmanNode::HuffmanNode(HuffmanNode *l, HuffmanNode *r)']]]
